fd2816616235f8ca3e42ae461c5da08826ee2417
Anthony Jamora <anjamora@gmail.com>
2015-02-07 15:54:28 -0500

    Adding some animation so something exciting comes from my end

---
+++ PhoenixGame/Assets/Scripts/Player/playerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private float directionDampTime = .25f;
+	
+	public float speed = 0.0f;
+	private float h = 0.0f;
+	private float v = 0.0f;
+	
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		if (animator.layerCount >= 2) {
+			animator.SetLayerWeight(1,1);		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animator) {
+			
+			h = Input.GetAxis("Horizontal");
+			v = Input.GetAxis("Vertical");
+			
+			speed = new Vector2(h,v).sqrMagnitude;
+			
+			animator.SetFloat("Speed", speed);
+			animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+		}
+	}
+}
