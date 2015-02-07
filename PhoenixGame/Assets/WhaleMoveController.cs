51920d5c766258119a53b35c5251b7b445f24f7e
Jonny Nabors <jonnynabors@gmail.com>
2015-02-07 01:23:22 -0500

    I made a moonwalking whale. It's better if you just look at it instead of reading about the commit.

---
+++ PhoenixGame/Assets/WhaleMoveController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhaleMoveController : MonoBehaviour {
+	public Transform[] targetWaypoints;
+
+	private NavMeshAgent navagent;
+	public float swimSpeed = 2f;
+	public float moveWaitTime = 1f;
+
+	private int waypointDestination = 0;
+	private float moveTimer = 0f;
+	// Use this for initialization
+	void Awake () {
+		navagent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Patrol();
+	}
+
+	void Patrol()
+	{
+		navagent.speed = swimSpeed;
+		if(navagent.remainingDistance < navagent.stoppingDistance)
+		{
+			if(waypointDestination == targetWaypoints.Length - 1)
+				waypointDestination = 0;
+			else
+				waypointDestination++;
+
+		}
+		for(int i = 1; i < 3; i++)
+		{
+			navagent.destination = targetWaypoints[waypointDestination].position;
+		}
+	}
+}
