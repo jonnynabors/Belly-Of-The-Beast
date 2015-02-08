5325770f534dad1ded81b4f6cd6c8137865a7fc6
Jonny Nabors <jonnynabors@gmail.com>
2015-02-08 13:40:55 -0500

    Initial concept of Day/Night cycle is there. The game starts off at the end of the day, then gets dark, then gets light again. Has some low quality shadows implemented into it. Background music is from the Unity store, takes like 15 seconds or so to actually hear anything since it starts quiet. The whale now sits lower in the ocean and will (tragically) die if you touch him and stop moving.

---
+++ PhoenixGame/Assets/Scripts/GameController/WhaleMoveController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhaleMoveController : MonoBehaviour {
+
+	public float swimSpeed = 2f;
+	public Transform[] targetWaypoints;
+	public Animation anima;
+	public AudioClip triggerAudioEvent;
+
+	private NavMeshAgent navagent;
+	private int waypointDestination;
+	private GameObject player;
+	private GameObject whale;
+	private bool isDead = false;
+
+	// Use this for initialization
+	void Awake () {
+		navagent = GetComponent<NavMeshAgent>();
+		player = GameObject.FindGameObjectWithTag(Tags.player);
+		whale = GameObject.FindGameObjectWithTag(Tags.whale);
+		anima = whale.GetComponent<Animation>(); //Get build in animation components
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Call the patrol method
+		if(!isDead)
+		{
+			Patrol();
+		}
+	}
+
+	void Patrol()
+	{
+		//set the public value for the swim speed of the whale
+		navagent.speed = swimSpeed;
+
+		if(navagent.remainingDistance < navagent.stoppingDistance)
+		{
+			//If you are at the end of the array, restart
+			if(waypointDestination == targetWaypoints.Length - 1)
+				waypointDestination = 0;
+			else
+				waypointDestination++;
+		}
+			//Propel whale towards next waypoint location
+			navagent.destination = targetWaypoints[waypointDestination].position;
+	}
+
+	//On trigger event for character colliding with whale object
+	void OnTriggerEnter(Collider other)
+	{
+		//If collision occurs with player
+		if(other.gameObject == player)
+		{
+			//AudioSource.PlayClipAtPoint(triggerAudioEvent, transform.position);
+			//Play death animation
+			anima.Play ("death");
+			whale.rigidbody.velocity = Vector3.zero; //Stop movement
+			isDead = true; //Set flag of is dead to true
+		}
+	}
+
+}
--- PhoenixGame/Assets/Scripts/Unapplied/Tags.cs
+++ PhoenixGame/Assets/Scripts/Unapplied/Tags.cs
@@ -5,4 +5,6 @@ public class Tags : MonoBehaviour {
 
 	public const string player = "Player";
 	public const string whale = "Whale";
+	public const string sun = "Sun";
+	public const string moon = "Moon";
 }
