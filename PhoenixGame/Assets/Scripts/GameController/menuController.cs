a8e2dd72c758541a1ab3d124f527774a334839e1
Anthony Jamora <anjamora@gmail.com>
2015-02-09 12:12:18 -0500

    Revert "Created in game menu with "ESC" button feature"
    This reverts commit 51a37bb716278413b8301a39a9629e5a6680d693.

--- PhoenixGame/Assets/Scripts/GameController/menuController.cs
+++ PhoenixGame/Assets/Scripts/GameController/menuController.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class mainMenu : MonoBehaviour {
+public class menuController : MonoBehaviour {
 
 	public Canvas quitMenu;
 	public Button startText;
@@ -38,7 +38,7 @@ public class mainMenu : MonoBehaviour {
 	public void StartLevel()
 	{
 		//loads first level
-		Application.LoadLevel ("IslandLevel01");
+		Application.LoadLevel (1);
 	}
 
 	public void ExitGame()
