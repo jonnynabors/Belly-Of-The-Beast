80a35634bf87f9fa965a625e0179aa7d80e74781
Anthony Jamora <anjamora@gmail.com>
2015-02-28 12:38:52 -0500

    ladder package

---
+++ PhoenixGame/Assets/Scripts/Objects/climbLadder.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class climbLadder : MonoBehaviour {
+
+	public ThirdPersonController charController;
+	public bool inside = false;
+	public float charHeight = 0;
+	public Transform charTransform;
+
+	// Use this for initialization
+	void Start () {
+		charController = GetComponent (ThirdPersonController);
+	}
+
+	void OnTriggerEnter(Collider Col)
+	{
+		if(Col.gameObject.tag == "Ladder")
+		{
+			charController.enabled = false;
+			inside = !inside;
+		}
+	}
+
+	void OnTriggerExit(Collider Col)
+	{
+		if(Col.gameObject.tag == "Ladder")
+		{
+			charController.enabled = true;
+			inside = !inside;
+		}
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (inside == true && Input.GetKey ("w")) {
+			charTransform.transform.position += Vector3.up/charHeight;	
+			
+		}
+	}
+}
