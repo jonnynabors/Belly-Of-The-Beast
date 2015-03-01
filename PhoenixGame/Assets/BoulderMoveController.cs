648a1cda5628c9f0b23e5555f5dc554c8933b80f
Jonny Nabors <jonnynabors@gmail.com>
2015-03-01 18:56:38 -0500

    Added new game island and colliders. Gave physics to the boulder. New game music and water.

---
+++ PhoenixGame/Assets/BoulderMoveController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ * Script written by Jonny Nabors, 3/1/2015. Purpose of this script 
+ * is to give the boulder free movement when the player comes into 
+ * contact with the boulder, or when the puzzle is successfully solved.
+ */
+public class BoulderMoveController : MonoBehaviour {
+	private GameObject player;
+	private GameObject boulder;
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag(Tags.player);
+		boulder = GameObject.FindGameObjectWithTag(Tags.boulder);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//If collision occurs with player
+		if(other.gameObject == player)
+		{
+			//Remove Kinematic property from Rigidbody component.
+			rigidbody.isKinematic = false;
+			rigidbody.angularDrag = 10;
+		}
+	}
+}
--- PhoenixGame/Assets/Scripts/Unapplied/Tags.cs
+++ PhoenixGame/Assets/Scripts/Unapplied/Tags.cs
@@ -7,4 +7,5 @@ public class Tags : MonoBehaviour {
 	public const string whale = "Whale";
 	public const string sun = "Sun";
 	public const string moon = "Moon";
+	public const string boulder = "Boulder";
 }
