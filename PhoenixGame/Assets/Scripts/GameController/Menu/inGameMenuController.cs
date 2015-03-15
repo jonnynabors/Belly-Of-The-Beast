fecbd323b569cb830a2c8a3977743273cfde4b81
Anthony Jamora <anjamora@gmail.com>
2015-03-15 04:55:16 -0400

    update master scene with menus

--- PhoenixGame/Assets/Scripts/GameController/Menu/inGameMenuController.cs
+++ PhoenixGame/Assets/Scripts/GameController/Menu/inGameMenuController.cs
@@ -2,91 +2,115 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class menuController : MonoBehaviour {
-
+public class inGameMenuController : MonoBehaviour {
+	
 	public Canvas mainMenu;
 	public Canvas subMenu;
 	public Canvas quitMenu;
 	public GameObject optionsCanvas;
 	public GameObject controlsCanvas;
-	public GameObject creditsCanvas;
 	public GameObject exitCanvas;
-	public Button playButton;
+	public Button mainButton;
+	public Button returnButton;
 	public Button optionsButton;
 	public Button controlsButton;
-	public Button creditsButton;
 	public Button exitButton;
+	public ThirdPersonCamera cameraScript;
+	private bool isInMenu;
 
-
+	
+	
 	// Use this for initialization
 	void Start (){
 		//grab components
 		mainMenu = mainMenu.GetComponent<Canvas> ();
 		subMenu = subMenu.GetComponent<Canvas> ();
 		quitMenu = quitMenu.GetComponent<Canvas> ();
-		playButton = playButton.GetComponent<Button> ();
+		mainButton = mainButton.GetComponent<Button> ();
+		returnButton = returnButton.GetComponent<Button> ();
 		optionsButton = optionsButton.GetComponent<Button> ();
 		controlsButton = controlsButton.GetComponent<Button> ();
-		creditsButton = creditsButton.GetComponent<Button> ();
 		exitButton = exitButton.GetComponent<Button> ();
-
+		cameraScript = cameraScript.GetComponent<ThirdPersonCamera> ();
+		
 		//Makes quit menu NOT visible
+		isInMenu = false;
+		mainMenu.enabled = false;
 		quitMenu.enabled = false;
 		subMenu.enabled = false;
 	}
-
-	//playButton
-	public void playPress(){
-		Time.timeScale = 1;
-		//loads first level
-		Application.LoadLevel (1);
+	void Update ()
+	{
+		if(Input.GetKeyDown (KeyCode.Escape))
+		{
+			if (isInMenu)
+			{
+				isInMenu = false;
+				mainMenu.enabled = false;
+				cameraScript.enabled = true;
+				Time.timeScale = 1;
+			}
+			else
+			{
+				isInMenu = true;
+				mainMenu.enabled = true;
+				cameraScript.enabled = false;
+				Time.timeScale = 0;
+			}
+		}
 	}
 
+	public void mainPress(){
+		Time.timeScale = 0;
+		//loads first level
+		Application.LoadLevel (0);
+	}
+	
+	public void returnPress(){
+		Time.timeScale = 1;
+		cameraScript.enabled = true;
+		mainMenu.enabled = false;
+	}
+	
 	public void optionsPress(){
 		mainMenu.enabled = false;
 		subMenu.enabled = true;
 		optionsCanvas.SetActive (true);
 	}
-
+	
 	public void controlsPress(){
 		mainMenu.enabled = false;
 		subMenu.enabled = true;
 		controlsCanvas.SetActive (true);
 	}
-
-	public void creditsPress(){
-		mainMenu.enabled = false;
-		subMenu.enabled = true;
-		creditsCanvas.SetActive (true);
-	}
-
+	
 	//exitPress
 	public void ExitPress(){
 		//Makes quit menu visible and disables our main menu buttons
 		quitMenu.enabled = true;
-		playButton.enabled = false;
+		mainButton.enabled = false;
+		returnButton.enabled = false;
 		optionsButton.enabled = false;
 		controlsButton.enabled = false;
-		creditsButton.enabled = false;
 		exitButton.enabled = false;
 		exitCanvas.SetActive (true);
 	}
-
+	
 	//noButton Exit Warning
 	public void NoPress(){
 		//re-enable our main menu buttons and hide quitmenu
 		quitMenu.enabled = false;
-		playButton.enabled = true;
+		mainButton.enabled = true;
+		returnButton.enabled = true;
 		optionsButton.enabled = true;
 		controlsButton.enabled = true;
-		creditsButton.enabled = true;
 		exitButton.enabled = true;
 		exitCanvas.SetActive (false);
 	}
-
+	
 	//yesButton Exit Warning	
 	public void ExitGame(){
 		Application.Quit ();
 	}
-
+	
 }
