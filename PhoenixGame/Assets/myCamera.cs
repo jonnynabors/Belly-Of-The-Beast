6718b23aedc2992c61abacd977f925ecb429a149
Anthony Jamora <anjamora@gmail.com>
2015-03-28 22:00:07 -0400

    new temp player control

---
+++ PhoenixGame/Assets/myCamera.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myCamera : MonoBehaviour {
+
+	public float distanceAway = 1f;
+	public float distanceUp = 1f;
+	private float smooth = 5f;
+	public Transform follow;
+	public Vector3 targetPosition;
+	public Vector3 lookDir;
+	//position of camera
+	public Vector3 offset = new Vector3(0f, -0.72f, 0f);
+	
+	private Vector3 velocityCamSmooth = Vector3.zero;
+	private float camSmoothDampTime = 0.1f;
+	
+	// Use this for initialization
+	void Start () {
+		follow = GameObject.Find ("Player").GetComponent<Transform> ();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void LateUpdate(){
+		
+		Vector3 charOffset = follow.position + offset;
+
+		lookDir = charOffset - this.transform.position;
+		lookDir.y = 0;
+		lookDir.Normalize ();
+		Debug.DrawRay (this.transform.position, lookDir, Color.green);
+		
+		targetPosition = charOffset + follow.up * distanceUp - lookDir * distanceAway;
+		//targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+		//transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+		//		Debug.DrawRay (follow.position, Vector3.up * distanceUp, Color.red);
+		//		Debug.DrawRay (follow.position, 1f * follow.forward * distanceAway, Color.blue);
+		Debug.DrawLine (follow.position, targetPosition, Color.magenta);
+		
+		
+		
+		
+		smoothPosition(this.transform.position, targetPosition);
+		
+		transform.LookAt (follow);
+		
+	}
+	
+	private void smoothPosition(Vector3 fromPos, Vector3 toPos){
+		
+		this.transform.position = Vector3.SmoothDamp (fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
+		
+		
+	}
+	
+}
---
+++ PhoenixGame/Assets/myControllerAnim.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myControllerAnim : MonoBehaviour {
+
+	public Animator animator;
+	private Transform transform;
+	public myCamera mainCam;
+	private float speed = 2.0f;
+	public float direction = 0.0f;
+	private float angle = 0.0f;
+	public float moveX;
+	public float moveY;
+	public float angleRootToMove;
+	
+	
+	public float s;
+	private float d;
+	
+	private float time = 0f;
+	
+	private bool isWalking = false;
+	private bool isRunning = false;
+	private bool isSprinting = false;
+	private bool isMoving = false;
+	private bool isPivot = false;
+	
+	public float speedDampTime = 0.1f; 
+	public float directionDampTime = 0.1f;
+	public float directionSpeed = 3.0f;
+	
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform> ();
+		animator = GetComponent<Animator> ();
+		
+		if (animator.layerCount >= 2) {
+			animator.SetLayerWeight (1, 1);		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//change direction
+		Vector3 eulerAngles = transform.eulerAngles;
+		//eulerAngles.y = 90;
+		//eulerAngles.y = direction;
+		//transform.eulerAngles = eulerAngles;
+		
+		
+		moveX = Input.GetAxis ("Horizontal");
+		moveY = Input.GetAxis ("Vertical");
+		
+		s = new Vector2 (moveX, moveY).sqrMagnitude;
+		
+		
+		StickToWorldspace (this.transform, mainCam.transform, ref direction, ref speed);
+		animator.SetFloat ("s", setSpeed(speed));
+		animator.SetFloat ("Direction", direction, directionDampTime, Time.deltaTime);
+		
+	}
+	
+	public void StickToWorldspace(Transform root, Transform camera, ref float directionOut, ref float speedOut){
+		Vector3 rootDirection = root.forward;
+		Vector3 stickDirection = new Vector3 (moveX, 0, moveY);
+		
+		speedOut = stickDirection.sqrMagnitude;
+		
+		Vector3 CameraDirection = camera.forward;
+		CameraDirection.y = 0.0f;
+		Quaternion referentialShift = Quaternion.FromToRotation (Vector3.forward, CameraDirection);
+		
+		Vector3 moveDirection = referentialShift * stickDirection;
+		Vector3 axisSign = Vector3.Cross (moveDirection, rootDirection);
+		
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.red);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), stickDirection, Color.blue);
+		
+		
+		
+		angleRootToMove = Vector3.Angle (rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
+		
+		angleRootToMove /= 180f;
+		
+		directionOut = angleRootToMove * directionSpeed;
+		
+	}
+	private float setSpeed(float speed){
+		if (Input.GetButton ("Walk")) {
+			speed *= 1;
+			return speed;
+		} 
+		else if(Input.GetButton ("Sprint")){
+			speed *= 3;
+			return speed;
+		}
+		else{
+			speed *= 2;
+			return speed;
+		}
+	}
+}
