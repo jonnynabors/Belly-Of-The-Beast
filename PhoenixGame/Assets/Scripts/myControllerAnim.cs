31088c66c42969c029a441c4409fb2b672b8591e
Jonny Nabors <jonnynabors@gmail.com>
2015-04-21 16:45:11 -0400

    Small camera fixes and script cleanups.

--- PhoenixGame/Assets/Scripts/myControllerAnim.cs
+++ PhoenixGame/Assets/Scripts/myControllerAnim.cs
@@ -1,31 +1,10 @@
 ﻿/// <summary>
 /// UnityTutorials - A Unity Game Design Prototyping Sandbox
 /// <copyright>(c) John McElmurray and Julian Adams 2013</copyright>
-/// 
-/// UnityTutorials homepage: https://github.com/jm991/UnityTutorials
-/// 
-/// This software is provided 'as-is', without any express or implied
-/// warranty.  In no event will the authors be held liable for any damages
-/// arising from the use of this software.
-///
-/// Permission is granted to anyone to use this software for any purpose,
-/// and to alter it and redistribute it freely, subject to the following restrictions:
-///
-/// 1. The origin of this software must not be misrepresented; you must not
-/// claim that you wrote the original software. If you use this software
-/// in a product, an acknowledgment in the product documentation would be
-/// appreciated but is not required.
-/// 2. Altered source versions must be plainly marked as such, and must not be
-/// misrepresented as being the original software.
-/// 3. This notice may not be removed or altered from any source distribution.
-/// </summary>
 
 using UnityEngine;
 using System.Collections;
 
-/// <summary>
-/// #DESCRIPTION OF CLASS#
-/// </summary>
 public class myControllerAnim: MonoBehaviour 
 {
 	
@@ -116,7 +95,7 @@ public class myControllerAnim: MonoBehaviour
 //		isInTheAir = IsInTheAir ();
 
 		//Calculate if player is currently falling.
-		if (rigid.velocity.y < -2) {
+		if (rigid.velocity.y < -4) {
 			fallTime += Time.deltaTime;
 			animator.SetBool("isFalling", true);
 			animator.applyRootMotion = false;
